a5c29229c0ecc337c2de464eb47f0bcd0f3682e7
Manuel <102795174+GH-NotManuTv@users.noreply.github.com>
2024-02-15 13:34:12 +0100

    Update config.lua

--- config.lua
+++ config.lua
@@ -2,15 +2,15 @@ Config = {}
 
 Config.Lang = "en" -- it = Italiano, en = English
 
-Config.Colore_Embed = 16777215
+Config.Colore_Embed = 16777215 -- https://gist.github.com/thomasbnt/b6f455e2c7d743b796917fa3c205f812 for all colors
 Config.Immagine = "https://cdn.discordapp.com/attachments/1008116358631915580/1008416668608889005/Senza-titolo-1.gif"
 
 -- Entrate || Join Server
 
-Config.Entrate_Titolo = "ENTRATE"
-Config.Entrate_Weebhook = "https://discord.com/api/webhooks/1207123943736082492/YZia_myaFeM9bwLdSslF621h5ag4Nvdqse9-pIGRsd3G4yDA81JRkBTb8ik34rpJ0PSz"
+Config.Entrate_Titolo = "" -- Title
+Config.Entrate_Weebhook = "" -- Webhook
 
 -- Uscite || Leave Server
 
-Config.Uscite_Titolo = "USCITE"
-Config.Uscite_Weebhook = "https://discord.com/api/webhooks/1207124120538718228/ejLl7x1Sfib8j9oggAW22pDBB1ogXo3gfQacII9zfpfVNk4CxW8M2CEWCRSTGz7m7Fxm"
+Config.Uscite_Titolo = "" -- Title 
+Config.Uscite_Weebhook = "" -- Webhook
